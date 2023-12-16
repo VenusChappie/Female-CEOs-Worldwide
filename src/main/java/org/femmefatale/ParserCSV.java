38a845bc2300779f041678026d9c783d6ce25124
Afro_dite_codes <venuschappie@gmail.com>
2023-12-16 19:23:19 +0200

    Added ParserCSV class and imported CSV data using method

---
+++ src/main/java/org/femmefatale/ParserCSV.java
@@ -0,0 +1,36 @@
+package org.femmefatale;
+
+import java.io.*;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ParserCSV {
+
+    public static ArrayList<String> validData(File csvFile) throws IOException {
+
+        BufferedReader br = new BufferedReader(new FileReader(csvFile));
+        String line = "";
+        String[] tempArr;
+        ArrayList<String> mainArr = new ArrayList<>();
+
+        while ((line = br.readLine()) != null) {
+
+            tempArr = line.split(",");
+
+            for (String item : tempArr)  {
+                mainArr.add(item);
+            }
+            mainArr.add("\n");
+        }
+        return mainArr;
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        String path ="/Users/venuschappie/Desktop/student_work/Female-CEOs-Worldwide/Female-CEOs/TestData - Sheet1.csv";
+        System.out.println(validData(new File(path)));
+    }
+
+
+}
---
+++ src/main/java/org/femmefatale/TestDataLine.java
@@ -0,0 +1,30 @@
+package org.femmefatale;
+
+public class TestDataLine {
+
+    String testData =
+            "Walmart,1,0.0,572754.0,13673.0,2300000.0,Retailing,Bentonville,AR,no,no,no,yes,1.0,C. Douglas McMillon,https://www.stock.walmart.com,WMT,352037\n" +
+            "Amazon,2,0.0,469822.0,33364.0,1608000.0,Retailing,Seattle,WA,no,no,no,yes,2.0,Andrew R. Jassy,www.amazon.com,AMZN,1202717\n" +
+            "Apple,3,0.0,365817.0,94680.0,154000.0,Technology,Cupertino,CA,no,no,no,yes,3.0,Timothy D. Cook,www.apple.com,AAPL,2443962\n" +
+            "CVS Health,4,0.0,292111.0,7910.0,258000.0,Health Care,Woonsocket,RI,no,no,yes,yes,4.0,Karen Lynch,https://www.cvshealth.com,CVS,125204\n" +
+            "UnitedHealth Group,5,0.0,287597.0,17285.0,350000.0,Health Care,Minnetonka,MN,no,no,no,yes,5.0,Andrew P. Witty,www.unitedhealthgroup.com,UNH,500468\n" +
+            "Exxon Mobil,6,4.0,285640.0,23040.0,63000.0,Energy,Irving,TX,no,no,no,yes,10.0,Darren W. Woods,www.exxonmobil.com,XOM,371841\n" +
+            "Berkshire Hathaway,7,-1.0,276094.0,89795.0,372000.0,Financials,Omaha,NE,no,no,no,yes,6.0,Warren E. Buffett,www.berkshirehathaway.com,BRKA,625468\n" +
+            "Alphabet,8,1.0,257637.0,76033.0,156500.0,Technology,Mountain View,CA,no,no,no,yes,9.0,Sundar Pichai,https://www.abc.xyz,GOOGL,1309359\n" +
+            "McKesson,9,-2.0,238228.0,-4539.0,67500.0,Health Care,Irving,TX,no,no,no,no,7.0,Brian S. Tyler,www.mckesson.com,MCK,47377\n" +
+            "AmerisourceBergen,10,-2.0,213988.8,1539.9,40000.0,Health Care,Conshohocken,PA,no,no,no,yes,8.0,Steven H. Collis,www.amerisourcebergen.com,ABC,29972\n" +
+            "Costco Wholesale,11,1.0,195929.0,5007.0,288000.0,Retailing,Issaquah,WA,no,no,no,yes,12.0,W. Craig Jelinek,www.costco.com,COST,230443\n" +
+            "Cigna,12,1.0,174078.0,5365.0,72963.0,Health Care,Bloomfield,CT,no,no,no,yes,13.0,David Cordani,https://www.cigna.com,CI,88459\n" +
+            "AT&T,13,-2.0,168864.0,20081.0,202600.0,Telecommunications,Dallas,TX,no,no,no,yes,11.0,John T. Stankey,www.att.com,T,148907\n" +
+            "Microsoft,14,1.0,168088.0,61271.0,181000.0,Technology,Redmond,WA,no,no,no,yes,15.0,Satya Nadella,www.microsoft.com,MSFT,1941033\n" +
+            "Cardinal Health,15,-1.0,162467.0,611.0,46827.0,Health Care,Dublin,OH,no,no,no,yes,14.0,Jason Hollar,www.cardinalhealth.com,CAH,15169\n" +
+            "Chevron,16,11.0,162465.0,15625.0,42595.0,Energy,San Ramon,CA,no,no,no,yes,27.0,Michael K. Wirth,www.chevron.com,CVX,284132\n" +
+            "Home Depot,17,1.0,151157.0,16433.0,490600.0,Retailing,Atlanta,GA,no,no,no,yes,18.0,Edward P. Decker,www.homedepot.com,HD,308152\n" +
+            "Walgreens Boots Alliance,18,-2.0,148579.0,2542.0,258500.0,Food & Drug Stores,Deerfield,IL,no,no,yes,yes,16.0,Roz Brewer,www.walgreensbootsalliance.com,WBA,33360\n" +
+            "Marathon Petroleum,19,13.0,141032.0,9738.0,17700.0,Energy,Findlay,OH,no,no,no,yes,32.0,Michael J. Hennigan,www.marathonpetroleum.com,MPC,47526\n" +
+            "Elevance Health,20,3.0,138639.0,6104.0,98200.0,Health Care,Indianapolis,IN,no,no,yes,yes,23.0,Gail K. Boudreaux,www.elevancehealth.com,ELV,119923\n" +
+            "Kroger,21,-4.0,137888.0,1655.0,420000.0,Food & Drug Stores,Cincinnati,OH,no,no,no,yes,17.0,W. Rodney McMullen,www.thekrogerco.com,KR,33846\n" +
+            "Ford Motor,22,-1.0,136341.0,17937.0,183000.0,Motor Vehicles & Parts,Dearborn,MI,no,no,no,yes,21.0,James D. Farley Jr.,www.ford.com,F,50609\n" +
+            "Verizon Communications,23,-3.0,133613.0,22065.0,118400.0,Telecommunications,New York,NY,no,no,no,yes,20.0,Hans E. Vestberg,www.verizon.com,VZ,211872\n" +
+            "JPMorgan Chase,24,-5.0,127202.0,48334.0,271025.0,Financials,New York,NY,no,no,no,yes,19.0,James Dimon,www.jpmorganchase.com,JPM,336469\n";
+}
